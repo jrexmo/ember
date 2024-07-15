513264828cf4198c07cecb85732657e9e75252be
Thorne Wolfenbarger <thornecanyon@gmail.com>
2024-07-14 18:59:49 -0700

    move webapp templates to proper directory

--- src/ember/webapp/server.py
+++ src/ember/webapp/server.py
@@ -41,7 +41,7 @@ async def lifespan(_: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 # Serve static files
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="src/ember/webapp/templates"), name="static")
 
 
 def delete_files_after_delay(files: list[pathlib.Path], delay=DEFAULT_DELETE_DELAY):
